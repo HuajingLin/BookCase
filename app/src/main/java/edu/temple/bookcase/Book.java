b2aeff262421b3a8402f6118430802fcd6d1d514
Huajing Lin <tuj68316@temple.edu>
2019-11-06 10:08:07 -0500

    add book class.

---
+++ app/src/main/java/edu/temple/bookcase/Book.java
@@ -0,0 +1,10 @@
+package edu.temple.bookcase;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int published; //The year the book was published
+    private String coverURL;//the image representing the book cover
+
+}
